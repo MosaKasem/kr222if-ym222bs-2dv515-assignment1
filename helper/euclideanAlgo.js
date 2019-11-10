15d60b6361bcdea806556f7f26bcf60824fbcd92
MosaKasem <thescorpion119@gmail.com>
2019-11-10 09:05:02 +0100

    fix

--- helper/euclideanAlgo.js
+++ helper/euclideanAlgo.js
@@ -8,62 +8,45 @@ function euclidean(userID, userB) {
     // console.log('userB: ', userB);
     // console.log('userB: ', ...userB);
 
-    let sim = 0
+    let simularityScore = 0
     let n = 0
     let newArr = [];
     let euclideanObj = {}
-    
-    const userA = ratings.filter(e => e.UserID === userID) // get user A ratings
-    // const filteredList = ratings.filter(e => e.UserID === userB) // everyone except root user
 
-    // console.log('filteredList: ', filteredList);
-    
+    const userA = ratings.filter(e => e.UserID === userID) // get user A ratings
+
     for (let rootUser of userA) {
         for (let currentUser of userB) {
-            if (rootUser.UserID === currentUser.UserID) {
-                "Its same"
-            }
             if (rootUser.Movie === currentUser.Movie) { // if its same movie
                 // sim += getSimilarity(parseFloat(rootUser.Rating), parseFloat(currentUser.Rating))
-                sim += Math.pow(rootUser.Rating - currentUser.Rating, 2)
+                simularityScore += parseFloat((rootUser.Rating - currentUser.Rating) ** 2)
                 n += 1
             }
         }
     }
-    console.log(sim)
-    // console.log(ratings)
-    // console.log(newArr)
+    if (n <= 0) {
+        return 0
+    }
+    return 1 / (1 + simularityScore)
 }
+function getSimularity(userID) {
+    let grouped = [];
+    ratings.forEach(function (a) {
+        this[a.UserID] || grouped.push(this[a.UserID] = []);
+        this[a.UserID].push(a);
+    }, Object.create(null));
 
-let grouped = [];
-
-ratings.forEach(function (a) {
-this[a.UserID] || grouped.push(this[a.UserID] = []);
-this[a.UserID].push(a);
-}, Object.create(null));
-
-for (let i = 0; i < grouped.length; i++) {
-    // grouped[i];
-    if ("1" !== grouped[i][0].UserID) {
-        euclidean("1", grouped[i])
+    for (let i = 0; i < grouped.length; i++) {
+        if (userID !== grouped[i][0].UserID) {
+            euclidean(userID, grouped[i])
+        }
     }
 }
 
-function doEuclidean(rootUser) {
-    // find similarity between root user and for each other user
-    let activeUser;
-    // ratings.map(e => {
-    //     if (e.UserID !== rootUser)
-    //     euclidean(rootUser, e.UserID)
-    // })
-}
-// euclidean("4")
 
-
-
-function getSimilarity(a,b) {
-    if(a === 0) { return b;}
-    if(b === 0) { return a;}
+function getSimilarity(a, b) {
+    if (a === 0) { return b; }
+    if (b === 0) { return a; }
 
     // decrease and conqure - recursion
     return getSimilarity(b, a % b);
