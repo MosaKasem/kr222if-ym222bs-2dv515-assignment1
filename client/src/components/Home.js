5ededcaffb824440ea6890ba301b5a8d72fe6e52
Yasmin <ym222bs@student.lnu.se>
2019-11-11 22:15:14 +0100

    client can get data from server

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,33 +1,49 @@
-import React, {Fragment} from 'react'
-
+import React, { Fragment, useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+    const [ user, setMovie ] = useState('')
+    
+    const updateAccordingToUser = (e) => {
+        // console.log('event: ', e)
+        // console.log('user:  ',user)
+        fetchMovies(e)
+    }
+
+    const handleEnterKey = (e) => {
+        if(e.key === 'Enter') {
+            fetchMovies()
+        }
+    }
+
+    const fetchMovies = async (e) => {
+        console.log(e.target.value)
+        try {
+            const response = await axios.get(`/ratings/${e.target.value}`);
+            console.log(response);
+          } catch (error) {
+            console.error(error);
+          }
+    }
+
     return (
         <Fragment>
            <form>
-               <div>
-               <div className="dropdown">
-                    <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
+               <div className="input-group mb-3">
+                    <div className="input-group-prepend">
                     </div>
-                    <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                    </div>
-               </div>
+                    <input type="text" value={user} onChange={updateAccordingToUser} onKeyPress={handleEnterKey} className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                </div>
+                <button type="button" className="btn btn-primary">Search</button>
             </form> 
+            <ul>
+                <li>
+
+                </li>
+            </ul>
         </Fragment>
     )
 }
 
 export default Home
+
